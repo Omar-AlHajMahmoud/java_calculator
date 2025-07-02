18b9e2c5eac985175cda733c19509ae2a6a49016
i6400902 <i6400902>
2025-07-02 18:11:11 +0300

    gui fixes and added functionality to the gui

--- src/main/java/gui/CalculatorManager.java
+++ src/main/java/gui/CalculatorManager.java
@@ -1,21 +1,93 @@
 package gui;
 
+import engine.CalculatorEngine;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import operations.binaryOperations.Addition;
+import operations.binaryOperations.Division;
+import operations.binaryOperations.Multiplication;
+import operations.binaryOperations.Subtraction;
+
+import javafx.event.ActionEvent;
+
+/**
+ * @Author Omar Mahmoud
+ */
 public class CalculatorManager {
-    public void handleNumberInput(double number, double[] numbers) {
-        if (numbers[0] == 0) {
-            numbers[0] = number;
-        }
 
-        if (numbers[1] == 0) {
-            numbers[1] = number;
+    private static final CalculatorEngine calculatorEngine = new CalculatorEngine();
+    private static final Addition addition = new Addition();
+    private static final Subtraction subtraction = new Subtraction();
+    private static final Multiplication multiplication = new Multiplication();
+    private static final Division division = new Division();
+
+    private double currentNumber = 0;
+    private String currentOperator = "";
+    private boolean waitingForOperand = false;
+
+    public void handleNumberClick (Button button, TextArea textArea) {
+        String number = button.getText();
+        appendToDisplay(number, textArea);
+    }
+
+    private void appendToDisplay (String number, TextArea textArea) {
+        if (waitingForOperand) {
+            textArea.setText(number);
+            waitingForOperand = false;
+        } else {
+            String current = textArea.getText();
+            if (current.equals("0")) {
+                textArea.setText(number);
+            } else {
+                textArea.setText(current + number);
+            }
         }
     }
 
-    public void handleOperatorInput(char operator) {
-
+    public void handleOperatorClick (Button button, TextArea textArea) {
+        String operator = button.getText();
+        processOperator(operator, textArea);
     }
 
-    public void handleEqualInput() {
+    private void processOperator (String operator, TextArea textArea) {
+        double number = Double.parseDouble(textArea.getText());
 
+        if (!currentOperator.isEmpty() && !waitingForOperand) {
+            double result = performCalculation(currentNumber, number, operator);
+            textArea.setText(Double.toString(result));
+            currentNumber = result;
+        } else {
+            currentNumber = number;
+        }
+
+        currentOperator = operator;
+        waitingForOperand = true;
+    }
+
+    private double performCalculation (double currentNumber, double number, String operator) {
+        return switch (operator) {
+            case "+" -> calculatorEngine.calculate(addition, currentNumber, number);
+            case "-" -> calculatorEngine.calculate(subtraction, currentNumber, number);
+            case "X" -> calculatorEngine.calculate(multiplication, currentNumber, number);
+            case "/" -> calculatorEngine.calculate(division, currentNumber, number);
+            default -> number;
+        };
+    }
+
+    public void handleEqualsClick (TextArea textArea) {
+        if (!currentOperator.isEmpty() && !waitingForOperand) {
+            double secondNumber = Double.parseDouble(textArea.getText());
+            double result = performCalculation(currentNumber, secondNumber, currentOperator);
+            textArea.setText(Double.toString(result));
+            currentOperator = "";
+            waitingForOperand = true;
+        }
+    }
+
+    public void handleClearClick (TextArea textArea) {
+        textArea.setText("0");
+        currentNumber = 0;
+        currentOperator = "";
+        waitingForOperand = false;
     }
 }
--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -9,25 +9,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import operations.BinaryOperation;
-import operations.binaryOperations.*;
-import operations.unaryOperations.SquareRoot;
 
 public class Home extends Application {
 
-    private double number_1, number_2;
-    private String currentOperator;
-    private boolean number1_is_empty = true;
-    private boolean number2_is_empty = true;
-
-    CalculatorManager calculatorManager = new CalculatorManager();
-    Addition addition = new Addition();
-    Subtraction subtraction = new Subtraction();
-    Multiplication multiplication = new Multiplication();
-    Division division = new Division();
-    Power power = new Power();
-    Modulus modulus = new Modulus();
-    SquareRoot squareRoot = new SquareRoot();
+    private final CalculatorManager calculatorManager = new CalculatorManager();
 
     private TextArea textArea;
 
@@ -41,143 +26,91 @@ public class Home extends Application {
 
         StackPane root = new StackPane();
 
-        VBox vbox = new VBox();
-        vbox.setSpacing(10);
-        vbox.setPadding(new Insets(10));
-
-        textArea = new TextArea();
-        textArea.setEditable(false);
-
-        HBox firstRow = new HBox();
-        firstRow.setSpacing(10);
-        firstRow.setPadding(new Insets(10));
-
-        Button seven = new Button("7");
-        seven.setPrefWidth(60);
-        seven.setOnAction(e -> {appendNumber("7");});
-        Button eight = new Button("8");
-        eight.setPrefWidth(60);
-        eight.setOnAction(e -> {appendNumber("8");});
-        Button nine = new Button("9");
-        nine.setPrefWidth(60);
-        nine.setOnAction(e -> {appendNumber("9");});
-        Button multiply = new Button("X");
-        multiply.setPrefWidth(60);
-        multiply.setOnAction(e -> {setOperator("*");});
-        multiply.setOnAction(e -> {textArea.appendText("X");});
-
-        firstRow.getChildren().addAll(seven, eight, nine, multiply);
-
-        HBox secondRow = new HBox();
-        secondRow.setSpacing(10);
-        secondRow.setPadding(new Insets(10));
-
-        Button four = new Button("4");
-        four.setPrefWidth(60);
-        four.setOnAction(e -> {appendNumber("4");});
-        Button five = new Button("5");
-        five.setPrefWidth(60);
-        five.setOnAction(e -> {appendNumber("5");});
-        Button six = new Button("6");
-        six.setPrefWidth(60);
-        six.setOnAction(e -> {appendNumber("6");});
-        Button minus = new Button("-");
-        minus.setPrefWidth(60);
-        minus.setOnAction(e -> {setOperator("-");});
-        minus.setOnAction(e -> {textArea.appendText("-");});
-
-        secondRow.getChildren().addAll(four, five, six, minus);
-
-        HBox thirdRow = new HBox();
-        thirdRow.setSpacing(10);
-        thirdRow.setPadding(new Insets(10));
-
-        Button one = new Button("1");
-        one.setPrefWidth(60);
-        one.setOnAction(e -> {appendNumber("1");});
-        Button two = new Button("2");
-        two.setPrefWidth(60);
-        two.setOnAction(e -> {appendNumber("2");});
-        Button three = new Button("3");
-        three.setPrefWidth(60);
-        three.setOnAction(e -> {appendNumber("3");});
-        Button add = new Button("+");
-        add.setPrefWidth(60);
-        add.setOnAction(e -> {setOperator("+");});
-        add.setOnAction(e -> {textArea.appendText("+");});
-
-        thirdRow.getChildren().addAll(one, two, three, add);
-
-        HBox fourthRow = new HBox();
-        fourthRow.setSpacing(10);
-        fourthRow.setPadding(new Insets(10));
-
-        Button divide = new Button("/");
-        divide.setPrefWidth(60);
-        divide.setOnAction(e -> {setOperator("/");});
-        divide.setOnAction(e -> {textArea.appendText("/");});
-        Button zero = new Button("0");
-        zero.setPrefWidth(60);
-        zero.setOnAction(e -> {appendNumber("0");});
-        Button dot = new Button(".");
-        dot.setPrefWidth(60);
-        dot.setOnAction(e -> {textArea.appendText(".");});
-        Button equal = new Button("=");
-        equal.setPrefWidth(60);
-        equal.setOnAction(e -> {calculate(number_1, number_2);});
-
-        fourthRow.getChildren().addAll(divide, zero, dot, equal);
-
-        HBox fifthRow = new HBox();
-        fifthRow.setSpacing(10);
-        fifthRow.setPadding(new Insets(10));
-
-        vbox.getChildren().addAll(textArea, firstRow, secondRow, thirdRow, fourthRow, fifthRow);
-        root.getChildren().add(vbox);
+        root.getChildren().add(initializeCalculator());
 
         Scene scene = new Scene(root, 300, 400);
+        scene.getStylesheets().add(getClass().getResource("/gui/styles.css").toExternalForm());
 
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
 
-    private void appendNumber(String number) {
-        textArea.appendText(number);
-        if (number1_is_empty) {
-            number1_is_empty = false;
-            number_1 = Double.parseDouble(number);
-        } else if (number2_is_empty) {
-            number2_is_empty = false;
-            number_2 = Double.parseDouble(number);
-        }
+    private VBox initializeCalculator() {
+        Button add = createOperatorButton("+");
+        Button subtract = createOperatorButton("-");
+        Button multiply = createOperatorButton("X");
+        Button divide = createOperatorButton("/");
+        Button clear = createClearButton();
+        Button equal = createEqualButton();
+        Button zero = createNumberButton("0");
+        Button one = createNumberButton("1");
+        Button two = createNumberButton("2");
+        Button three = createNumberButton("3");
+        Button four = createNumberButton("4");
+        Button five = createNumberButton("5");
+        Button six = createNumberButton("6");
+        Button seven = createNumberButton("7");
+        Button eight = createNumberButton("8");
+        Button nine = createNumberButton("9");
+
+        HBox firstRow = new HBox();
+        HBox secondRow = new HBox();
+        HBox thirdRow = new HBox();
+        HBox fourthRow = new HBox();
+
+        firstRow.getChildren().addAll(seven, eight, nine, add);
+        secondRow.getChildren().addAll(four, five, six, subtract);
+        thirdRow.getChildren().addAll(one, two, three, multiply);
+        fourthRow.getChildren().addAll(clear, zero, equal, divide);
+
+        textArea = new TextArea();
+        textArea.setEditable(false);
+        textArea.getStyleClass().add("text-area");
+
+        VBox root = new VBox();
+        root.getChildren().addAll(textArea, firstRow, secondRow, thirdRow, fourthRow);
+
+        return root;
     }
 
-    private void setOperator(String operator) {
-        currentOperator = operator;
+    private Button createNumberButton(String text) {
+        Button button = new Button(text);
+        button.setPrefWidth(80);
+        button.setPrefHeight(55);
+        button.setPadding(new Insets(10));
+        button.getStyleClass().add("number-button");
+        button.setOnAction(event -> {calculatorManager.handleNumberClick(button, textArea);});
+        return button;
     }
 
-    private void calculate(double number_1, double number_2) {
-        double result = 0.0;
-        switch (currentOperator) {
-            case "+":
-                result = addition.calculate(number_1, number_2);
-                break;
-            case "-":
-                result = subtraction.calculate(number_1, number_2);
-                break;
-            case "*":
-                result = multiplication.calculate(number_1, number_2);
-                break;
-            case "/":
-                result = division.calculate(number_1, number_2);
-                break;
-            case "^":
-                result = power.calculate(number_1, number_2);
-                break;
-            default:
-                break;
-        }
-        textArea.appendText(Double.toString(result));
+    private Button createOperatorButton(String text) {
+        Button button = new Button(text);
+        button.setPrefWidth(80);
+        button.setPrefHeight(55);
+        button.setPadding(new Insets(10));
+        button.getStyleClass().add("operator-button");
+        button.setOnAction(event -> {calculatorManager.handleOperatorClick(button, textArea);});
+        return button;
+    }
+
+    private Button createClearButton() {
+        Button button = new Button("C");
+        button.setPrefWidth(80);
+        button.setPrefHeight(55);
+        button.setPadding(new Insets(10));
+        button.getStyleClass().add("clear-button");
+        button.setOnAction(event -> {calculatorManager.handleClearClick(textArea);});
+        return button;
+    }
+
+    private Button createEqualButton() {
+        Button button = new Button("=");
+        button.setPrefWidth(80);
+        button.setPrefHeight(55);
+        button.setPadding(new Insets(10));
+        button.getStyleClass().add("operator-button");
+        button.setOnAction(event -> calculatorManager.handleEqualsClick(textArea));
+        return button;
     }
 }
